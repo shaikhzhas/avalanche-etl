2387deb40ae773abc37734f497d18145039ea0eb
Zhas <zhas@MacBook-Air-Zhanar.local>
2022-11-09 22:57:10 +0600

    get block range for timestamps for avalanche

--- avalancheetl/cli/get_block_range_for_timestamps.py
+++ avalancheetl/cli/get_block_range_for_timestamps.py
@@ -1,5 +1,6 @@
 # MIT License
 #
+# Modifications Copyright (c) Zhassulan Shaikhygali, shaikh.zhas@gmail.com
 # Copyright (c) 2018 Evgeny Medvedev, evge.medvedev@gmail.com
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -23,33 +24,29 @@
 
 import click
 
-from ethereumetl.web3_utils import build_web3
-
 from blockchainetl.file_utils import smart_open
 from blockchainetl.logging_utils import logging_basic_config
-from ethereumetl.providers.auto import get_provider_from_uri
-from ethereumetl.service.eth_service import EthService
-from ethereumetl.utils import check_classic_provider_uri
+from avalancheetl.web3_utils import build_web3
+from avalancheetl.providers.auto import get_provider_from_uri
+from avalancheetl.service.ava_service import AvaService
+from avalancheetl.utils import check_classic_provider_uri
 
 logging_basic_config()
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.option('-p', '--provider-uri', default='https://mainnet.infura.io', show_default=True, type=str,
-              help='The URI of the web3 provider e.g. '
-                   'file://$HOME/Library/Ethereum/geth.ipc or https://mainnet.infura.io')
+@click.option('-p', '--provider-uri', required = True, type=str, help='The URI of the web3 provider e.g.')
 @click.option('-s', '--start-timestamp', required=True, type=int, help='Start unix timestamp, in seconds.')
 @click.option('-e', '--end-timestamp', required=True, type=int, help='End unix timestamp, in seconds.')
 @click.option('-o', '--output', default='-', show_default=True, type=str, help='The output file. If not specified stdout is used.')
-@click.option('-c', '--chain', default='ethereum', show_default=True, type=str, help='The chain network to connect to.')
-def get_block_range_for_timestamps(provider_uri, start_timestamp, end_timestamp, output, chain='ethereum'):
+@click.option('-c', '--chain', default='avalance', show_default=True, type=str, help='The chain network to connect to.')
+def get_block_range_for_timestamps(provider_uri, start_timestamp, end_timestamp, output, chain='avalanche'):
     """Outputs start and end blocks for given timestamps."""
     provider_uri = check_classic_provider_uri(chain, provider_uri)
     provider = get_provider_from_uri(provider_uri)
     web3 = build_web3(provider)
-    eth_service = EthService(web3)
-
-    start_block, end_block = eth_service.get_block_range_for_timestamps(start_timestamp, end_timestamp)
+    ava_service = AvaService(web3)
+    start_block, end_block = ava_service.get_block_range_for_timestamps(start_timestamp, end_timestamp)
 
     with smart_open(output, 'w') as output_file:
         output_file.write('{},{}\n'.format(start_block, end_block))
